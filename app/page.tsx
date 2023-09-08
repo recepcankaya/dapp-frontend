042710f32b365fb00595ff5ada95a14594eba23c
Recep Cankaya <recepcnky@outlook.com>
2023-09-08 21:35:40 +0300

    create a new branch and install tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 "use client";
-import { ConnectWallet } from "@thirdweb-dev/react";
-import styles from "../styles/Home.module.css";
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <main className={styles.main}></main>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
 
 export default Home;
