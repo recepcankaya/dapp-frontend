07eac8ccbd2dd2caabad0d025e092e00ef607036
Recep Cankaya <recepcnky@outlook.com>
2023-09-09 12:39:09 +0300

    finsih setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <p className="">Hello world!</p>;
 };
 
 export default Home;
