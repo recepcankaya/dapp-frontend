77ec444819f44e64dd6904208466c61c7210a1b1
Recep Cankaya <recepcnky@outlook.com>
2023-10-01 07:34:58 +0300

    feat: finish responsive from navbar to about

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,16 +2,15 @@ import Link from "next/link";
 import CustomButton from "./CustomButton";
 
 export default function NavBar() {
-  // @todo - navbarın yüksekliğini responsive yap
   return (
-    <nav className="w-full h-44 bg-gradient-to-b from-[#574E70]/75 to-[#0A002F]/0">
+    <nav className="w-full h-28 lg:h-40 bg-gradient-to-b from-[#574E70]/75 to-[#0A002F]/0">
       <div className="h-full pt-5 flex justify-between">
         <h1 className="ml-6 text-3xl">Ladderit</h1>
         <ul className="w-1/2 mr-3 flex justify-between text-sm sm:text-base md:text-xl lg:text-2xl italic font-bold">
-          <Link href="" className="">
+          <Link href="" className="opacity-0 md:opacity-100">
             Contact
           </Link>
-          <Link href="" className="">
+          <Link href="" className="opacity-0 md:opacity-100">
             About Us
           </Link>
           <CustomButton
