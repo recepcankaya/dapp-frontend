6f5d298423bacfaac7307f3eca6d64db15be9278
Recep Cankaya <recepcnky@outlook.com>
2023-10-05 21:38:42 +0300

    feat: add metatga and change herosection animation

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,24 +4,16 @@ import Image from "next/image";
 import bubbleBg from "../public/images/hero-bg.png";
 
 export default function HeroSection() {
-  const { ref: firstH2Ref, inView: isFirstH2Visible } = useInView();
   const { ref: secondH2Ref, inView: isSecondH2Visible } = useInView();
 
   return (
-    <header
-      ref={firstH2Ref}
-      className="relative h-[400px] lg:h-[847px] font-bold">
+    <header className="relative h-[400px] lg:h-[847px] font-bold">
       <Image
         src={bubbleBg}
         alt="bubbles"
         className="absolute top-0 left-0 w-full h-full opacity-10 object-cover"
       />
-      <h2
-        className={`${
-          isFirstH2Visible
-            ? "text-2xl sm:text-4xl lg:text-5xl font-holtwood text-center pt-12 lg:absolute lg:top-32 lg:left-28 animate-visibleHeroH2"
-            : "text-2xl sm:text-4xl lg:text-5xl font-holtwood text-center pt-12 lg:absolute lg:top-32 lg:left-28 opacity-0"
-        }`}>
+      <h2 className="text-2xl sm:text-4xl lg:text-5xl font-holtwood text-center pt-12 lg:absolute lg:top-32 lg:left-28 animate-visibleHeroH2">
         WHEN YOU LADDER IT
       </h2>
       <h2
