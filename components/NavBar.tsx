4b6bddb00895d682b0153f15914033c4fcffbd45
Recep Cankaya <recepcnky@outlook.com>
2023-10-05 20:14:20 +0300

    feat: adding logo

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link";
+import Image from "next/image";
+import logo from "../public/images/logo.jpg";
 import { Button } from "./ui/Button";
 
 export default function NavBar() {
   return (
     <nav className="w-full h-28 lg:h-40 bg-gradient-to-b from-[#574E70]/75 to-[#0A002F]/0">
-      <div className="h-full pt-5 flex justify-between">
-        <h1 className="ml-6 text-3xl">Ladderit</h1>
-        <ul className="w-1/2 mr-3 flex justify-between text-sm sm:text-base md:text-xl lg:text-2xl italic font-bold">
+      <div className="h-full flex justify-around items-center">
+        <Image
+          src={logo}
+          alt="logo"
+          className="w-20 h-20 md:w-24 md:h-24 lg:h-32 lg:w-32 object-cover rounded-xl"
+        />
+        <ul className="w-1/2 mr-3 flex justify-between items-center text-sm sm:text-base md:text-xl lg:text-2xl italic font-bold">
           <Link href="/contact" className="opacity-0 md:opacity-100">
             Contact
           </Link>
