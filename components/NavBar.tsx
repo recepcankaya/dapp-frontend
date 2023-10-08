e3bf047a567b52b105ffb66de5cd7641fa492a97
Recep Cankaya <recepcnky@outlook.com>
2023-10-08 15:57:18 +0300

    feat: add button animation to navbar and make cards draggable in About section

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
+import { motion } from "framer-motion";
+
 import logo from "../public/images/logo.jpg";
 import { Button } from "./ui/Button";
 
@@ -13,15 +15,26 @@ export default function NavBar() {
           className="w-20 h-20 md:w-24 md:h-24 lg:h-32 lg:w-32 2xl:h-44 2xl:w-44 object-cover rounded-xl"
         />
         <ul className="w-1/2 mr-3 flex justify-between items-center text-sm sm:text-base md:text-xl lg:text-2xl 2xl:text-4xl italic font-bold">
-          <Link href="/contact" className="opacity-0 md:opacity-100">
-            Contact
-          </Link>
-          <Link href="" className="opacity-0 md:opacity-100">
-            About Us
-          </Link>
-          <Button asChild>
-            <Link href="/login">Log In</Link>
-          </Button>
+          <li>
+            <Link href="/contact" className="opacity-0 md:opacity-100">
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link href="" className="opacity-0 md:opacity-100">
+              About Us
+            </Link>
+          </li>
+          <motion.li
+            whileHover={{
+              scale: 1.1,
+              transition: { duration: 0.2 },
+            }}
+            whileTap={{ scale: 0.9 }}>
+            <Button asChild>
+              <Link href="/login">Log In</Link>
+            </Button>
+          </motion.li>
         </ul>
       </div>
     </nav>
