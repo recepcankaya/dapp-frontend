f74646c3257f42f7af4def5c0948c152340db807
Akif Emre Reis <akifemre611@hotmail.com>
2024-06-09 17:01:47 +0300

    feat: user ui menü entegrasyonu yapıldı, kullanıcıya campaign olmadığı zaman carousel, menü olmadığı zaman menü butonu gösterilmiyor

--- src/components/customer/menu/RenderMenu.tsx
+++ src/components/customer/menu/RenderMenu.tsx
@@ -57,50 +57,78 @@ export default function RenderMenu({ menu }: Props) {
   return (
     <>
       {menu ? (
-        <div className="max-w-sm">
-          <ul
-            className="flex gap-8 overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide"
-            onMouseDown={handleMouseDown}
-            onMouseLeave={handleMouseLeave}
-            onMouseUp={handleMouseUp}
-            onMouseMove={handleMouseMove}>
-            {menu.map((item: CategoryProduct) => (
-              <li
-                key={item.categoryID}
-                onClick={() => setSelectedTab(item.category)}
-                className="inline-block select-none"
-                style={{
-                  textDecoration:
-                    selectedTab === item.category ? "underline" : "none",
-                  textDecorationThickness: "1px",
-                  textUnderlineOffset: "4px",
-                }}>
-                {item.category}
-              </li>
-            ))}
-          </ul>
-          <ul className="mt-12">
+        <div className="flex flex-col justify-center items-center w-full">
+          <div className="flex flex-col justify-center mt-20 gap-y-2">
+            <p className="font-bold mb-3 pl-4 sm:pl-5">Menü</p>
+            <div
+              className="mx-auto grid grid-cols-3 gap-8 justify-center border border-[#DBB5B5] p-5 relative"
+              style={{
+                borderWidth: "1em",
+                maxWidth: "90%",
+              }}
+            >
+              <span className="absolute top-0 left-0 translate-x-[-75%] translate-y-[-75%] w-[20px] h-[20px] rounded-full bg-[#D9D9D9]"></span>
+              <span className="absolute top-0 left-0 translate-x-[-86.5%] translate-y-[-86%] w-[14px] h-[14px] rounded-full bg-[#987070]"></span>
+              <span className="absolute top-0 right-0 translate-x-[75%] translate-y-[-75%] w-[20px] h-[20px] rounded-full bg-[#D9D9D9]"></span>
+              <span className="absolute top-0 right-0 translate-x-[86%] translate-y-[-86%] w-[14px] h-[14px] rounded-full bg-[#987070]"></span>
+              {menu.map((item: CategoryProduct) => (
+                <li
+                  key={item.categoryID}
+                  onClick={() => setSelectedTab(item.category)}
+                  className="inline-block select-none italic hover:cursor-pointer drop-shadow-2xl"
+                  style={{
+                    textDecoration:
+                      selectedTab === item.category ? "underline" : "none",
+                    textDecorationThickness: "1px",
+                    textUnderlineOffset: "4px",
+                  }}
+                >
+                  {item.category}
+                </li>
+              ))}
+            </div>
+          </div>
+          <div className="mt-12 w-[90vw] flex-col">
+            <div className="flex-col justify-start ml-1 mb-8">
+              <p className="font-bold mt-10">Fiyatlar</p>
+              <p className="text-[#00000084]">{selectedTab}</p>
+            </div>
             {menu
               .filter((item: CategoryProduct) => selectedTab === item.category)
               .flatMap((item: CategoryProduct) =>
                 item.products?.map((product: Product) => (
-                  <li key={product.id}>
-                    <p>{product.name}</p>
-                    <p>{product.price}</p>
-                    <p>{product.description}</p>
-                    {/* <Image
-            src={product.image.replace(
-              "ipfs://",
-              "https://ipfs.io/ipfs/"
-            )}
-            alt={product.name}
-            width={100}
-            height={100}
-          /> */}
-                  </li>
+                  <>
+                    <li
+                      className="flex items-start justify-between overflow-hidden mb-12"
+                      key={product.id}
+                    >
+                      <Image
+                        className="rounded-full"
+                        src={product.image.replace(
+                          "ipfs://",
+                          "https://ipfs.io/ipfs/"
+                        )}
+                        alt={product.name}
+                        width={60}
+                        height={60}
+                        style={{
+                          aspectRatio: "60/60",
+                        }}
+                      />
+                      <div className="flex flex-col w-1/2 justify-center h-[100%]">
+                        <p className="font-bold" title={product.name}>
+                          {product.name}
+                        </p>
+                        <p className="break-all" title={product.description}>
+                          {product.description}
+                        </p>
+                      </div>
+                      <p className="font-semibold">{product.price}</p>
+                    </li>
+                  </>
                 ))
               )}
-          </ul>
+          </div>
         </div>
       ) : (
         <p>Menü bulunamadı</p>
