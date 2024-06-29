cc3f5fcf2ced9960faf0ba592448506f08bc0090
Recep Cankaya <recepcnky@outlook.com>
2024-06-29 16:11:53 +0300

    feat: creating initial schema for menus table

---
+++ supabase/migrations/20240629130614_create_menu_table.sql
@@ -0,0 +1,26 @@
+CREATE TABLE public.menus (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  branch_id UUID REFERENCES public.brand_branch(id) ON DELETE CASCADE,
+  name TEXT NOT NULL,
+  description TEXT,
+  price NUMERIC,
+  image_url TEXT,
+  category TEXT NOT NULL,
+  position INT NOT NULL,
+);
+
+ALTER TABLE "public"."menus" ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Enable ALL access for Branches based on id to menus table"
+ON "public"."menus"
+TO authenticated
+USING ( (SELECT auth.uid()) = branch_id ) 
+WITH CHECK ( (SELECT auth.uid()) = branch_id );
+
+CREATE POLICY "Enable read access for all users for menus table"
+ON "public"."menus"
+FOR SELECT
+TO anon, authenticated
+USING (
+  true
+);
