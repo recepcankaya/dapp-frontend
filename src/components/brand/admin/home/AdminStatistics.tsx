7da48c3d542fcd93524689d77bb5af7fba35904e
elfbrtc <elif.barutcu.5353@hotmail.com>
2024-07-02 18:33:24 +0300

    -update branch card design -add selectedBranch props for update branch statics

--- src/components/brand/admin/home/AdminStatistics.tsx
+++ src/components/brand/admin/home/AdminStatistics.tsx
@@ -1,13 +1,17 @@
 "use client";
-import {AdminBrandBranchInfo, type AdminHomeStatistics } from "@/src/lib/types/jsonQuery.types";
+import React, { useState } from "react";
+import {
+  AdminBrandBranchInfo,
+  AdminHomeStatistics,
+} from "@/src/lib/types/jsonQuery.types";
 import AdminHomeCards from "./AdminHomeCards";
 import AdminLineChart from "./AdminLineChart";
 import AdminHomeBranchCard from "./AdminHomeBranchCard";
 
 type AdminStatisticsProps = {
-  brandData: AdminBrandBranchInfo,
-  requiredNumberForFreeRight: Brand["required_number_for_free_right"];
-  weeklyTotalOrders: BrandBranch["weekly_total_orders"];
+  brandData: AdminBrandBranchInfo;
+  requiredNumberForFreeRight: number;
+  weeklyTotalOrders: any;
   calculatedData: AdminHomeStatistics;
 };
 
@@ -17,12 +21,48 @@ export default function RenderAdminStatistics({
   weeklyTotalOrders,
   calculatedData,
 }: AdminStatisticsProps) {
+  const [branchCalculatedData, setBranchCalculatedData] = useState(calculatedData);
+  const [selectedBranch, setSelectedBranch] = useState<string | null>(null);
+
+  const handleBranchSelect = (branchName: string | null) => {
+    if (branchName === selectedBranch) {
+      setSelectedBranch(null);
+      setBranchCalculatedData(calculatedData);
+    } else {
+      setSelectedBranch(branchName);
+      if (branchName) {
+        const selectedBranchData = brandData.brand_branch.find(
+          (branch) => branch.branch_name === branchName
+        );
+        if (selectedBranchData) {
+          const updatedCalculatedData = {
+            total_orders: selectedBranchData.total_orders,
+            total_used_free_rights: selectedBranchData.total_used_free_rights,
+            total_unused_free_rights:
+              selectedBranchData.total_unused_free_rights,
+            daily_total_orders: selectedBranchData.daily_total_orders,
+            daily_total_used_free_rights:
+              selectedBranchData.daily_total_used_free_rights,
+            monthly_total_orders: selectedBranchData.monthly_total_orders,
+          };
+
+          setBranchCalculatedData(updatedCalculatedData);
+        }
+      }
+    }
+  };
+
   return (
     <main className="flex flex-col gap-8 p-8 md:p-12 lg:p-16 text-[#000101]">
-      <AdminHomeBranchCard brandData = {brandData}/>
+      <AdminHomeBranchCard
+        brandData={brandData}
+        onBranchSelect={handleBranchSelect}
+        selectedBranch={selectedBranch}
+      />
       <AdminHomeCards
         requiredNumberForFreeRight={requiredNumberForFreeRight}
-        adminData={calculatedData}
+        adminData={branchCalculatedData}
+        selectedBranch={selectedBranch}
       />
       <AdminLineChart weeklyTotalOrders={weeklyTotalOrders} />
     </main>
