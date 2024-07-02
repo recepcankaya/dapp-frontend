f5f4d5ca742cc63b765bd335db4f77febede4f96
Recep Cankaya <recepcnky@outlook.com>
2024-07-02 14:47:21 +0300

    feat: removing custom font

--- src/components/customer/menu/RenderMenu.tsx
+++ src/components/customer/menu/RenderMenu.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Image from "next/image";
 
 import type { Product } from "@/src/lib/types/product.types";
@@ -26,17 +26,15 @@ export default function RenderMenu({ menus }: Props) {
             <div className="flex gap-8 justify-start overflow-y-auto bg-[#DBB5B5] p-6 relative drop-shadow-xl overflow-x-auto">
               {menusArray?.map((product) => (
                 <li
-                  key={product.category}
-                  onClick={() => setSelectedTab(product.category)}
-                  className="font-bold inline-block select-none hover:cursor-pointer drop-shadow-2xl font-lad-island-moments text-3xl min-w-fit"
+                  key={item.categoryID}
+                  onClick={() => setSelectedTab(item.category)}
+                  className="font-semibold inline-block select-none hover:cursor-pointer  min-w-fit"
                   style={{
                     textDecoration:
                       selectedTab === product.category ? "underline" : "none",
                     textUnderlineOffset: "4px",
-                    textShadow: "1px 3px 3px rgba(0, 0, 0, 0.5)",
-                  }}
-                >
-                  {product.category}
+                  }}>
+                  {item.category}
                 </li>
               ))}
             </div>
@@ -45,41 +43,35 @@ export default function RenderMenu({ menus }: Props) {
             <div className="flex-col justify-start ml-1 mb-8">
               <h2 className="font-bold mt-10 text-lg">Fiyatlar</h2>
             </div>
-            {menusArray
-              ?.filter((product) => product.category === selectedTab)
-              .map((product) => (
-                <>
-                  <li
-                    className="flex items-start justify-between overflow-hidden mb-12"
-                    key={product.id}
-                  >
-                    <div className="w-[70px] h-[70px] relative">
-                      <Image
-                        className="rounded-lg"
-                        src={
-                          product.image_url && product.image_url.length > 0
-                            ? product.image_url
-                            : "/"
-                        }
-                        alt={product.name}
-                        fill
-                      />
-                    </div>
-                    <div className="flex flex-col w-1/2 justify-center h-[100%]">
-                      <p className="font-bold" title={product.name}>
-                        {product.name}
-                      </p>
-                      <p
-                        className="break-all"
-                        title={product.description ?? undefined}
-                      >
-                        {product.description}
-                      </p>
-                    </div>
-                    <p className="font-semibold">{product.price}</p>
-                  </li>
-                </>
-              ))}
+            {menu
+              .filter((item: CategoryProduct) => selectedTab === item.category)
+              .flatMap((item: CategoryProduct) =>
+                item.products?.map((product: Product) => (
+                  <>
+                    <li
+                      className="flex items-start justify-between overflow-hidden mb-12"
+                      key={product.id}>
+                      <div className="w-[70px] h-[70px] relative">
+                        <Image
+                          className="rounded-lg"
+                          src={product.image}
+                          alt={product.name}
+                          fill
+                        />
+                      </div>
+                      <div className="flex flex-col w-1/2 justify-center h-[100%]">
+                        <p className="font-bold" title={product.name}>
+                          {product.name}
+                        </p>
+                        <p className="break-all" title={product.description}>
+                          {product.description}
+                        </p>
+                      </div>
+                      <p className="font-semibold">{product.price}</p>
+                    </li>
+                  </>
+                ))
+              )}
           </div>
         </div>
       ) : (
